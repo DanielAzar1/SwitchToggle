708c90d01ba30e08cd9c48ba440623ba48ad3172
Daniel azar <da6331@bs.amalnet.k12.il>
2024-02-26 16:10:25 +0200

    2nd commit

--- app/src/main/java/com/example/switchtoggle/MainActivity.java
+++ app/src/main/java/com/example/switchtoggle/MainActivity.java
@@ -36,6 +36,6 @@ public class MainActivity extends AppCompatActivity {
         if(!(check1.isChecked()) && check2.isChecked())
             cl.setBackgroundColor(Color.RED);
         if(!(check1.isChecked()) && !(check2.isChecked()))
-            cl.setBackgroundColor(Color.rgb(245,123,250));
+            cl.setBackgroundColor(Color.rgb(245,123,251));
     }
 }
